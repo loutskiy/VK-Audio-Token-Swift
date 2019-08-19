e5f969ef203eb55f2aa8796b6cfedb45a3393b04
Михаил Луцкий <mikhail@lwts.ru>
2019-08-19 23:05:36 +0300

    License

--- VKAudioToken/VkClient.swift
+++ VKAudioToken/VkClient.swift
@@ -6,6 +6,22 @@
 //  Copyright © 2019 Mikhail Lutskii. All rights reserved.
 //
 
+//    VKAudioToken - Token generate for access to VKontakte official audio.
+//    Copyright (C) 2019  LUTSKII MIKHAIL
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import Foundation
 
 public class VkClient {
